f4389e78f9d82a43003f0c67b5528083139ea7f1
Marco Jardim <marquinho.jardim@gmail.com>
2014-09-24 18:17:07 -0300

    Teste para ver se o socket está vivo
    se estiver morto no cliente, ele já avisa

--- client.js
+++ client.js
@@ -24,8 +24,11 @@ socket.on('data', function(data) {
 		// note:  d is an object, and when converted to a string it will
 		// end with a linefeed.  so we (rather crudely) account for that  
 		// with toString() and then substring() 
-			
-		socket.write(d.toString().substring(0, d.length-1));
+		if(socket.writable) {
+			socket.write(d.toString().substring(0, d.length-1));
+		} else {
+			console.log("socket died :(");
+		}
 	});
   
 }).on('end', function() {
