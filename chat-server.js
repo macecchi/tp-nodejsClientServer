d62130da80c0c39c6935a5d6219187e099cad845
Mario Cecchi <macecchi@gmail.com>
2015-10-25 22:01:49 -0200

    Atualiza sys.puts por console.log e melhora comandos pra controle

--- chat-server.js
+++ chat-server.js
@@ -4,25 +4,32 @@ var net = require('net');
 var sockets = [];
  
 var svr = net.createServer(function(sock) { // callback executado a cada criacao de socket, apos o ACK
-    sys.puts('Conectou-se: ' + sock.remoteAddress + ':' + sock.remotePort); 
+    console.log('Novo cliente conectou: ' + sock.remoteAddress + ':' + sock.remotePort); 
     sock.write('Oi ' + sock.remoteAddress + ':' + sock.remotePort + '\n'); // envia boas-vindas ao cliente
     sockets.push(sock); // adiciona na lista de sockets
  
     sock.on('data', function(data) {  // callback executado quando o cliente envia mensagem
-        if (data == 'EXIT') { // comando de finalizacao do cliente, fecha o socket
-            sys.puts('comando EXIT recebido: ' + sock.remoteAddress + ':' + sock.remotePort + '\n');
-            sock.destroy();
-            var idx = sockets.indexOf(sock);
-            if (idx != -1) {
-                delete sockets[idx]; // remove da lista de sockets
-            }
-            return;
-        }
+    	var receivedMessage = data.toString();
+
+    	// Verificar se a mensagem é um comando
+    	if (receivedMessage.charAt(0) == '/') {
+    		var command = receivedMessage.substring(1, receivedMessage.length).split(' ');
+	        if (command == 'exit') { // comando de finalizacao do cliente, fecha o socket
+	            console.log('Comando /exit recebido de ' + sock.remoteAddress + ':' + sock.remotePort + '\n');
+	            sock.destroy();
+	            var idx = sockets.indexOf(sock);
+	            if (idx != -1) {
+	                delete sockets[idx]; // remove da lista de sockets
+	            }
+	            return;
+	        }
+    	}
+
         var len = sockets.length;
         for (var i = 0; i < len; i ++) { // broadcast da mensagem recebida, envia para todos os clientes (sockets conhecidos)
             if (sockets[i] != sock) {
                 if (sockets[i]) {
-                    sockets[i].write(sock.remoteAddress + ':' + sock.remotePort + ':' + data);
+                    sockets[i].write(sock.remoteAddress + ':' + sock.remotePort + ':' + receivedMessage);
                 }
             }
         }
@@ -30,23 +37,24 @@ var svr = net.createServer(function(sock) { // callback executado a cada criacao
  
     sock.on('end', function(data) { // cliente disconecta
 		try {
-			sys.puts('Desconectou-se: ' + data + data.remoteAddress + ':' + data.remotePort + '\n');
+			console.log('Desconectou-se: ' + data + data.remoteAddress + ':' + data.remotePort + '\n');
 		} catch(err) {
-			sys.puts('Uma conexao foi abortada\n');
+			console.log('Uma conexao foi abortada\n');
 		}
         var idx = sockets.indexOf(sock);
         if (idx != -1) {
             delete sockets[idx];
         }
     });
+    
 	sock.on('error', function(err) { // exibe stack trace no caso de erros imprevistos
-		sys.puts('ECONNRESET?\n' + err.stack);
+		console.log('ECONNRESET?\n' + err.stack);
 		console.log(err);
 	});
 });
  
-var svraddr = '74.207.235.213'; // endereco do servidor, ex.: localhost, 127.0.0.1, 74.207.235.213
+var svraddr = '0.0.0.0'; // endereco do servidor, ex.: localhost, 127.0.0.1, 74.207.235.213
 var svrport = 8080; // porta para o ACK
 
 svr.listen(svrport, svraddr); // inicializa a porta de escuta
-sys.puts('Servidor criado em ' + svraddr + ':' + svrport + '\n'); // debug
+console.log('Servidor criado em ' + svraddr + ':' + svrport + '\n'); // debug
--- client.js
+++ client.js
@@ -17,20 +17,26 @@ console.log('Bem-vindo!');
 console.log('Digite EXIT para encerrar o chat');
 
 socket.on('data', function(data) { // Escuta o stream de dados que vem do servidor
-	sys.puts(data); // se chegou algo do servidor, escreve na tela
-}).on('connect', function() { // assim que recebe o evento de 'conectado', abre um console para o usuário
+	console.log(data.toString()); // se chegou algo do servidor, escreve na tela
+});
+
+socket.on('connect', function() { // assim que recebe o evento de 'conectado', abre um console para o usuário
 	var stdin = process.openStdin(); // abre console
 	stdin.addListener("data", function(data) { // listener para tratamento do que foi digitado no console
 		// data é um objeto "puro" javascript que precisa ser convertido pra string antes de usar
 		// além disso é preciso remover o line feed do final (por isso o substring)		
 		if(socket.writable) { // se o socket ainda estiver ativo, escreva nele o que foi digitado
+			var outMessage = data.toString().substring(0, data.length - 1);
+			//console.log('[DEBUG] Enviando para o server: ' + );
 			socket.write(data.toString().substring(0, data.length - 1)); // escrevendo
 		} else { // se o socket estiver fechado indevidamente (abortado, falha na rede, etc), avisar e sair do programa
 			console.log("O socket morreu :(");
 			process.exit(2);
 		}
 	});
-}).on('end', function() { // se o comando de finalização do socket for executado, escutar pelo evento de fechamento normal do socket originado do servidor
+});
+
+socket.on('end', function() { // se o comando de finalização do socket for executado, escutar pelo evento de fechamento normal do socket originado do servidor
 	console.log('Adeus...');
 	process.exit(1);
 });
